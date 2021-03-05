7eefc54de3b02267831e33df5f50ca252bf4c55d
alexestudio <80129697+alexestudio@users.noreply.github.com>
2021-03-05 16:37:16 -0600

    Delete general-modal.js

--- assets/js/general-modal.js
+++
@@ -1,6 +0,0 @@
-    // GENERAL MODAL
-    function modalGeneral (){
-      let modal = document.getElementById('modal');
-      modal.innerHTML = '';
-      modal.classList.toggle('w3-show');
-    }
