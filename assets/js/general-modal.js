50b4e0b41a0f7b64b7d36683481a61639a2c3442
alexestudio <80129697+alexestudio@users.noreply.github.com>
2021-03-05 16:26:47 -0600

    Create general-modal.js

---
+++ assets/js/general-modal.js
@@ -0,0 +1,6 @@
+    // GENERAL MODAL
+    function modalGeneral (){
+      let modal = document.getElementById('modal');
+      modal.innerHTML = '';
+      modal.classList.toggle('w3-show');
+    }
