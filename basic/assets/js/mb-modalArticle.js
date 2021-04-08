b6b9b0fd6baf231053a0a127434a1ac25e7bbf06
alexestudio86 <80129697+alexestudio86@users.noreply.github.com>
2021-04-07 23:03:55 -0500

    Update mb-modalArticle.js

--- basic/assets/js/mb-modalArticle.js
+++ basic/assets/js/mb-modalArticle.js
@@ -1,50 +1,49 @@
-      // CONVERT JSON TO JS OBJECT MOBILE
-      function modalArticleTemplate () {
+// CONVERT JSON TO JS OBJECT MOBILE
 
-        // <data:blog.homepageUrl.canonical/>
-        fetch('https://demo-basico.enmicel.com/' + 'feeds/posts/summary/' + this.getAttribute('data-ident') + '?alt=json')
-          .then(res => res.json())
-          .then(baseDeDatos => {
-
-          let modal = document.getElementById('modal');
-            modalContent = document.createElement('div');
-            modalContent.classList.add('w3-modal-content', 'w3-center', 'w3-card', 'w3-animate-bottom');
-              image = document.createElement('img')
-              image.setAttribute('alt', baseDeDatos.entry.title.$t);
-              image.setAttribute('src', (baseDeDatos.entry.media$thumbnail.url).replace('s72-c', 'w350-h250'));
-              image.style = 'width: 100%; height: 17em; object-fit: cover;';
-              exitBtn = document.createElement('a');
-              exitBtn.classList.add('w3-teal', 'w3-button', 'w3-large', 'w3-display-topright');
-              exitBtn.innerHTML = 'X';
-              exitBtn.addEventListener('click', modalGeneral);
-              header = document.createElement('h1');
-              header.classList.add('w3-container', 'w3-teal', 'w3-xlarge', 'w3-padding');
-              header.textContent = baseDeDatos.entry.title.$t;
-              content = document.createElement('p');
-              content.textContent = baseDeDatos.entry.summary.$t;
-              footer = document.createElement('div');
-              footer.classList.add('w3-large', 'w3-teal', 'w3-padding', 'price', 'bold');
-              footer.textContent = baseDeDatos.entry.category[0].term;
-
-            modal.appendChild(modalContent);
-              modalContent.appendChild(image);
-              modalContent.appendChild(exitBtn)
-              modalContent.appendChild(header);
-              modalContent.appendChild(content);
-              modalContent.appendChild(footer);
-
-        });
-      
-      }
+// Convert Json to js Object with ajax
+const getArticle = (e) => {
 
 
-      // EVENT ARTICLES
-      function eventsArticles () {
-        let articles = document.querySelectorAll('a[data-ident]');
-        for (let article of articles){
-          article.addEventListener('click', modalGeneral);
-          article.addEventListener('click', modalArticleTemplate);
-        }
-      }
+// Get Default Text (Para obtener el thumbnail de la imagen, es necesario que se suba sin importar el post)
+  fetch('feeds/posts/summary/' + e.currentTarget.getAttribute('data-ident') + '?alt=json')
+  .then(res => res.json())
+  .then(baseDeDatos => {
+  let modal = document.getElementById('modal');
+  modalContent = document.createElement('div');
+  modalContent.classList.add('w3-modal-content', 'w3-center', 'w3-card', 'w3-animate-bottom');
+    image = document.createElement('img')
+    image.setAttribute('alt', baseDeDatos.entry.title.$t);
+    image.setAttribute('src', baseDeDatos.entry.media$thumbnail.url.replace('s72-c','s500'));
+    image.style = 'width: 100%; height: 17em; object-fit: cover;';
+    exitBtn = document.createElement('a');
+    exitBtn.classList.add('w3-teal', 'w3-button', 'w3-large', 'w3-display-topright');
+    exitBtn.innerHTML = 'X';
+    exitBtn.addEventListener('click', modalGeneral);
+    header = document.createElement('h1');
+    header.classList.add('w3-container', 'w3-teal', 'w3-xlarge', 'w3-padding');
+    header.textContent = baseDeDatos.entry.title.$t;
+    content = document.createElement('p');
+    content.textContent = baseDeDatos.entry.summary.$t.replace('\n', '');
+    footer = document.createElement('div');
+    footer.classList.add('w3-large', 'w3-teal', 'w3-padding', 'price', 'bold');
+    footer.textContent = baseDeDatos.entry.category[0].term;
+  modal.appendChild(modalContent);
+    modalContent.appendChild(image);
+    modalContent.appendChild(exitBtn)
+    modalContent.appendChild(header);
+    modalContent.appendChild(content);
+    modalContent.appendChild(footer);
+  });
+}
 
-      eventsArticles();
+
+// Add events to articles
+const eventsArticles = () => {
+  let articles = document.querySelectorAll('a[data-ident]');
+  for (let article of articles){
+    article.addEventListener('click', modalGeneral);
+    article.addEventListener('click', getArticle);
+  }
+}
+
+eventsArticles();
