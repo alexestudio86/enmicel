2f24e8d897566c72261a4fec1afd9b010f3a3d8c
alexestudio86 <80129697+alexestudio86@users.noreply.github.com>
2021-08-24 11:19:07 -0500

    Update mb-sendData.js

--- platinum/js/mb-sendData.js
+++ platinum/js/mb-sendData.js
@@ -1,50 +1,30 @@
-// SEND DATA
-function sendData(){
-  whatsappNumber = '+5215562848048';
-  textIntro = '%5F'+encodeURIComponent('Hola, me gustaría ordenar')+'%5F%0A%0A';
-  carCompleto = car.map(carComplete).join('');
-  function carComplete(elemnt){
-    let elemento =  elemnt.cantidad+'%20x%20'+encodeURIComponent(elemnt.titulo)+'%0A';
-    return elemento;
-  };
-  finalText = '%0A'+encodeURIComponent('A nombre de: ');
-  nameValidated = document.querySelector('#nombre').value;
-  nameCleaned = '%2A'+encodeURIComponent(nameValidated)+'%2A';
-  finalMessage = document.querySelector('#finalMessage');
+// MB-SENDDATA
 
-  finalMessage.href = 'https://wa.me/'+whatsappNumber+'/?text='+textIntro+carCompleto+finalText+nameCleaned;
-  finalMessage.setAttribute('target', '_blank');
-  // Limpiar todo
-  setInterval(function(){
+// Send data
+const sendData = () => {
+  // Phone number
+  whatsappNumber = '+5215510698215';
+  // Intro
+  introText = '%5F'+encodeURIComponent('Hola, me gustaría ordenar')+'%5F%0A%0A';
+  // Body
+  bodyText = car.map( e => {
+    body = e.cantidad+'%20x%20'+encodeURIComponent(e.titulo)+'%0A';
+    return body;
+  }).join('');
+  // At name of
+  finalText = '%0A'+encodeURIComponent('A nombre de: ');
+  // Name validated
+  nameValidated = '%2A'+encodeURIComponent( document.getElementById('customerName').value )+'%2A';
+  sendMessage = modal.querySelector('a[name=btnContinue]');
+  sendMessage.setAttribute('target', '_blank');
+  sendMessage.href = 'https://wa.me/'+whatsappNumber+'/?text='+introText+bodyText+finalText+nameValidated;
+  // Clean all
+  setTimeout( () => {
     car.splice(0,car.length);
     localStorage.removeItem('car');
     modalGeneral();
     renderCar();
     location.reload(true);
-  },1000);
-
-}
-
-// VALIDATE NAME
-function validateName(){
-
-  textoNombre = document.querySelector('#textoNombre');
-  nameToValidate = document.querySelector('#nombre');
-  nameToReview = /^[A-z ]+$/.test(nameToValidate.value);
-
-  if((nameToValidate.value).length < 4){
-    textoNombre.classList.add('w3-text-red');
-    textoNombre.innerText = 'Nombre demasiado corto'
-  }else{
-    textoNombre.innerText = 'Ingresa tu nombre para continuar';
-    textoNombre.classList.remove('w3-text-red');
-    if(!nameToReview){
-      textoNombre.classList.add('w3-text-red');
-      textoNombre.innerText = 'Caracteres no permitidos (1-9,!"·$...)'
-    }else{
-      textoNombre.classList.remove('w3-text-red');
-      textoNombre.innerText = 'Gracias por su preferencia'
-      sendData();
-    }
-  }
+    }, 1000
+  );
 }
