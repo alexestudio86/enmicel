5868832ce4fb2aad92045ebe0f99ae16eef13b08
alexestudio86 <80129697+alexestudio86@users.noreply.github.com>
2021-08-26 13:39:25 -0500

    Update mb-toogleMenu.js

--- pro/components/App/mb-toogleMenu.js
+++ pro/components/App/mb-toogleMenu.js
@@ -1,14 +1,7 @@
-function barMenuElements(){
-    navBarMobileElements = document.querySelectorAll('#home > div > div > div > div > a');
-    for(element of navBarMobileElements){
-      element.addEventListener('click', closeBarMenu);
-    }
-  }
-  
-  function closeBarMenu(){
-    navBarMobileBars = document.querySelector('#menu-bar');
-    navBarMobileBars.checked = false;
-  }
-  
-  // BAR MENU ELEMENTS
-  barMenuElements();  
+// GENERAL MODAL TOGGLE
+
+function modalGeneral (){
+  let modal = document.getElementById('modal');
+  modal.innerHTML = '';
+  modal.classList.toggle('w3-show');
+}
