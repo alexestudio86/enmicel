98555e4fa4fb583b1e2f0658020777dd2faca82e
alexestudio86 <80129697+alexestudio86@users.noreply.github.com>
2021-09-06 14:14:59 -0500

    Update mb-dataForm.js

--- pro/components/App/mb-dataForm.js
+++ pro/components/App/mb-dataForm.js
@@ -16,7 +16,8 @@ const createDataForm = (e) => {
 
   // Add event to cotinue
   modal.querySelector('a[name=btnContinue]').addEventListener('click', validateData );
-}
 
-// Remove default
-document.querySelector('#modal form').preventDefault;
+  // Remove default
+  document.querySelector('#modal form').preventDefault();
+
+}
